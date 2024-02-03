8c739b5853bce76444002d5a5aa5a5436c1f9d99
araaiyan <araaiyan@tamu.edu>
2024-02-02 23:19:01 -0600

    Fix theme persistency
    -created applySavedTheme() to keep the theme persistent throughout pages/visits with a cookie

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 function button1() {
-    if(document.getElementById('page_style').getAttribute("href") === "style.css") {
+    if(localStorage.getItem("theme") === "light") {
         dark_mode();
     }
     else {
@@ -9,8 +9,23 @@ function button1() {
 
 function light_mode() {
     document.getElementById('page_style').setAttribute("href", "style.css");
+    localStorage.setItem("theme", "light");
 }
 
 function dark_mode() {
     document.getElementById('page_style').setAttribute("href", "style2.css");
+    localStorage.setItem("theme", "dark");
+}
+
+function applySavedTheme() {
+    //use when page loads to keep the theme persistent across pages/sessions
+    //don't know if lab covered a better way to do this because I was sick but oh well
+    const savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme === "dark") {
+        dark_mode();
+    } else {
+        //default
+        light_mode();
+    }
 }
